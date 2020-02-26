bd133a0614d3f9f515dc637052c7bf78b03104b7
chiqvu1 <chiquynhvu268@gmail.com>
2020-02-26 00:04:16 -0800

    - created Data class that will construct an ArrayList when Data object is created - created InsertRunnable to insert values from 0 - 10000 to object data. - created processRunnable to process integers from data, will display the thread name, id, and divisible by 7 true or false - created 3 threads in main, 1 thread to generating data and 2 threads to process data - create event listener for button to fire up threads when clicked

--- app/src/main/java/com/example/multithread/MainActivity.java
+++ app/src/main/java/com/example/multithread/MainActivity.java
@@ -3,12 +3,110 @@ package com.example.multithread;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.*;
 
 public class MainActivity extends AppCompatActivity {
-
+    //private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Data data = new Data();
+
+                InsertRunnable insertRunnable = new InsertRunnable(data);
+                Thread insertThread = new Thread(insertRunnable, "insertThread");
+
+                insertThread.start();
+
+                ProcessRunnable processRunnable1 = new ProcessRunnable(data);
+                ProcessRunnable processRunnabble2 = new ProcessRunnable(data);
+
+                Thread processThread1 = new Thread(processRunnable1, "processThread1");
+
+                processThread1.start();
+
+                Thread processThread2 = new Thread(processRunnabble2, "processThread2");
+
+                processThread2.start();
+            }
+        });
+
     }
 }
+
+class InsertRunnable implements Runnable {
+    Data data;
+    public InsertRunnable(Data data) {
+        this.data = data;
+    }
+
+    public void run() {
+        synchronized(data) {
+            int i = 0;
+            while (i < 2000) {
+                data.setData();
+                i++;
+            }
+        }
+    }
+}
+
+class ProcessRunnable implements Runnable {
+    Data data;
+    public ProcessRunnable(Data data) {
+        this.data = data;
+    }
+    public void run() {
+        try {
+            while(!data.getData().isEmpty()) {
+                int x = data.pullNum();
+                System.out.println(Thread.currentThread().getName() + " -----Process number: " + x +
+                                    " -----Divisible by 7: " + data.isDivisible(x));
+            }
+
+
+            //data.notifyAll();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class Data {
+    private ArrayList<Integer> list;
+
+    public Data() {
+        list = new ArrayList();
+    }
+
+    public void setData() {
+        list.add(new Random().nextInt(10000));
+    }
+
+    public ArrayList<Integer> getData() {
+        return list;
+    }
+
+    public int pullNum() throws InterruptedException {
+        int num = list.remove(0);
+        return num;
+    }
+
+    public boolean isDivisible(int num) {
+        boolean flag;
+        if (num % 7 == 0) {
+            flag = true;
+        } else {
+            flag = false;
+        }
+        return flag;
+    }
+}
+
